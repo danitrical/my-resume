52de05e054d565192c8a07ecb78085719f126bd1
Muhammad Danish Asim <danishasim403@gmail.com>
2024-09-22 03:41:54 -0400

    initiate my-resume with basic confgurations

---
+++ src/components/MainSection.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+export const MainSection = () => {
+  
+  return (
+  <div className="flex flex-row bg-transparent backdrop-blur-md w-full rounded-lg p-4 shadow-md text-white">
+    <div
+      className="w-1/2 text-left"
+    >
+      <h1 className="font-bold underline">
+        Hi there,
+      </h1>
+      <h1 className="font-light text-xl">
+        My name is
+      </h1>
+      <h1 className="font-bold text-5xl">
+        Muhammad Danish Asim
+      </h1>
+
+    </div>
+    <div
+      className="w-1/2"
+    >
+    </div>
+  </div>)
+}
